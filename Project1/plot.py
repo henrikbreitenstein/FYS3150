27157a716a6e0b6e5b2ad099ce164024dd497348
henrikbreitenstein <henrik.breitenstein@bremo.org>
2021-09-04 15:56:42 +0200

    eddit plot script

--- Project1/plot.py
+++ Project1/plot.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #opens file and loads the coloumns to x and y
-f = open('out.txt')
-x, y = np.loadtxt(f, unpack=True)
+x, y = np.loadtxt('out.txt', unpack=True)
+x2, v = np.loadtxt('solution.txt', unpack=True)
 
 #plots it with title and names on axes
 fig = plt.figure(figsize=(8,8))
 ax = plt.axes(xlabel='x', title='Exact Solution')
 plt.ylabel('u',rotation=0)
-ax.plot(x,y)
+ax.plot(x,y, label='Exact')
+ax.plot(x2, v, label='Approx')
+plt.legend()
 plt.show()
