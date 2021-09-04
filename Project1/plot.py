95bf731893d69b9db664e85333def7bbdbfdad5a
henrikbreitenstein <henrik.breitenstein@bremo.org>
2021-09-04 16:39:03 +0200

    added plots

--- Project1/plot.py
+++ Project1/plot.py
@@ -2,14 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #opens file and loads the coloumns to x and y
-x, y = np.loadtxt('out.txt', unpack=True)
-x2, v = np.loadtxt('solution.txt', unpack=True)
+for i in range(1, 10):
+    n = 150 + 10*i
+    x, y = np.loadtxt(f'out.txt', unpack=True)
+    x2, v = np.loadtxt(f'sol{n}.txt', unpack=True)
 
-#plots it with title and names on axes
-fig = plt.figure(figsize=(8,8))
-ax = plt.axes(xlabel='x', title='Exact Solution')
-plt.ylabel('u',rotation=0)
-ax.plot(x,y, label='Exact')
-ax.plot(x2, v, label='Approx')
-plt.legend()
-plt.show()
+    #plots it with title and names on axes
+    fig = plt.figure(figsize=(8,8))
+    ax = plt.axes(xlabel='x', title=f'Exact Solution vs Approx n = {n}')
+    plt.ylabel('u',rotation=0)
+    ax.plot(x,y, label='Exact')
+    ax.plot(x2, v, label='Approx')
+    plt.legend()
+    plt.savefig(f'plotexap{n}.jpg')
+    plt.show()
